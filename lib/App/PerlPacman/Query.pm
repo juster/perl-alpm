5005757cd2b410615d882ca87e1064aaa8f251ee
Justin Davis <jrcd83@gmail.com>
2010-05-25 13:22:42 -0700

    Convert filter method to return a closure.  Reorganized file.

--- lib/App/PerlPacman/Query.pm
+++ lib/App/PerlPacman/Query.pm
@@ -15,117 +15,68 @@ sub option_spec
         verbose|v debug root|r dbpath|b cachedir };
 }
 
-sub _print_info
+sub help
 {
-    my $pkg = shift;
+    return <<'END_HELP';
+usage:  ppacman {-Q --query} [options] [package(s)]
+options:
+  -c, --changelog      view the changelog of a package
+  -d, --deps           list packages installed as dependencies [filter]
+  -e, --explicit       list packages explicitly installed [filter]
+  -g, --groups         view all members of a package group
+  -i, --info           view package information (-ii for backup files)
+  -k, --check          check that the files owned by the package(s) are present
+  -l, --list           list the contents of the queried package
+  -m, --foreign        list installed packages not found in sync db(s) [filter]
+  -o, --owns <file>    query the package that owns <file>
+  -p, --file <package> query a package file instead of the database
+  -s, --search <regex> search locally-installed packages for matching strings
+  -t, --unrequired     list packages not required by any package [filter]
+  -u, --upgrades       list outdated packages [filter]
+  -q, --quiet          show less information for query and search
+      --config <path>  set an alternate configuration file
+      --logfile <path> set an alternate log file
+      --noconfirm      do not ask for any confirmation
+      --noprogressbar  do not show a progress bar when downloading files
+      --noscriptlet    do not execute the install scriptlet if one exists
+  -v, --verbose        be verbose
+      --debug          display debug messages
+  -r, --root <path>    set an alternate installation root
+  -b, --dbpath <path>  set an alternate database location
+      --cachedir <dir> set an alternate package cache location
+END_HELP
+}
 
-    my %info = $pkg->attribs;
-    $info{'desc'} = Text::Wrap::wrap( 'Description    : ',
-                                      '                 ',
-                                      $info{'desc'} );
+sub run_opts
+{
+    my ($class, $args_ref, %opts) = @_;
 
-    for my $pluralkey ( grep { /s\z/ } keys %info ) {
-        my $aref = $info{ $pluralkey };
-        $info{ $pluralkey } = ( @$aref ? join q{ }, @$aref : 'None' );
+    # Check if an unrecognized option is leftover...
+    my ($badopt) = grep { /\A-/ } @$args_ref;
+    $class->fatal( qq{unrecognized option: '$badopt'} ) if $badopt;
+
+    # 1. Convert arguments to package objects (or use all packages/groups)
+    # 2. Filter out packages based on command-line options
+    # 3. Print more info depending on command-line options
+    my ($converter, $defprinter) = $class->create_conv_print( %opts );
+    my $printer  = $class->create_printer( %opts ) || $defprinter;
+    my $filter   = $class->create_filter( %opts );
+
+    for my $pkg ( grep { $filter->() } $converter->( $args_ref ) ) {
+        $printer->( $pkg );
     }
 
-    $info{'reason'}        = ucfirst $info{'reason'} . 'ly installed.';
-    $info{'has_scriptlet'} = ( $info{'has_scriptlet'} ? 'Yes' : 'No' );
-
-    return <<"END_INFO";
-Name           : $info{'name'}
-Version        : $info{'version'}
-URL            : $info{'url'}
-Licenses       : $info{'licenses'}
-Groups         : $info{'groups'}
-Provides       : $info{'provides'}
-Depends On     : $info{'depends'}
-Optional Deps  : $info{'optdepends'}
-Required By    : $info{'requiredby'}
-Conflicts With : $info{'conflicts'}
-Replaces       : $info{'replaces'}
-Installed Size : $info{'isize'}
-Packager       : $info{'packager'}
-Architecture   : $info{'arch'}
-Build Date     : $info{'builddate'}
-Install Date   : $info{'installdate'}
-Install Reason : $info{'reason'}
-Install Script : $info{'has_scriptlet'}
-$info{'desc'};
-END_INFO
+    return 0;
 }
 
-sub create_printer
-{
-    my ($class, %opts) = @_;
-
-    my $printer_result;
-
-    my $append = sub {
-        my $newprinter = shift;
-        my $oldprinter = $printer_result || sub { };
-        $printer_result = sub {
-            my $obj = shift;
-            $newprinter->( $obj );
-            $oldprinter->( $obj );
-        }
-    };
-
-    my $printer = sub {
-        my $optname = shift;
-        return unless $opts{ $optname };
-        $append->( shift );
-    };
-
-    $printer->( 'info' => \&_print_info );
-    $printer->( 'changelog' => sub {
-                    my $pkg = shift;
-                    my $changelog = $pkg->changelog;
-                    if ( $changelog ) {
-                        print $changelog;
-                    }
-                    else {
-                        my $name = $pkg->name;
-                        __PACKAGE__->error
-                            ( q{no changelog available for } .
-                              qq{'$name'} );
-                    }
-                } );
-
-    return $printer_result;
-}
-
-sub _convert_args_to_objs
-{
-
-    my %param = @_;
-
-    my @found;
-
-    my $args_ref   = $param{'args'};
-    my $lookup_ref = $param{'converter'};
-    return $param{'defaults'}->() unless @$args_ref;
-
-    FINDOBJ_LOOP:
-    for my $objname ( @$args_ref ) {
-        my @objs = $lookup_ref->( $objname );
-        unless ( @objs ) {
-            __PACKAGE__->error( sprintf $param{'error'}, $objname );
-            next FINDOBJ_LOOP;
-        }
-        push @found, @objs;
-    }
-
-    return @found;
-}
+##----------------------------------------------------------------------------
+## CONVERTERS
+##----------------------------------------------------------------------------
 
 # Create a converter sub that translates arguments to objects.
 sub create_conv_print
 {
     my ($class, %opts) = @_;
-
-    use Dumpvalue;
-    Dumpvalue->new->dumpValue( \%opts );
 
     # Converts groups to a list of packages, the printer still remembers
     # which group the package came from...
@@ -137,7 +88,6 @@ sub create_conv_print
             my ($group_obj) = grep { $_->name eq $name }
                 ALPM->localdb->groups;
             return undef unless $group_obj;
-            print STDERR "Returning group packages\n";
 
             # This reverse lookup is used when printing packages...
             $group_of{ $_->name } = $group_obj->name
@@ -189,47 +139,146 @@ sub create_conv_print
     return ( $convert_ref, $printer_ref );
 }
 
-sub run_opts
+
+sub _convert_args_to_objs
 {
-    my ($class, $args_ref, %opts) = @_;
 
-    # Check if an unrecognized option is leftover...
-    my ($badopt) = grep { /\A-/ } @$args_ref;
-    $class->fatal( qq{unrecognized option: '$badopt'} ) if $badopt;
+    my %param = @_;
 
-    my ($converter, $defprinter) = $class->create_conv_print( %opts );
-    my $printer  = $class->create_printer( %opts ) || $defprinter;
-    my @packages = $converter->( $args_ref );
+    my @found;
 
-    filter_packages( \@packages, %opts );
-    $printer->( $_ ) foreach @packages;
+    my $args_ref   = $param{'args'};
+    my $lookup_ref = $param{'converter'};
+    return $param{'defaults'}->() unless @$args_ref;
 
-    return 0;
-}
-
-sub filter_packages
-{
-    my ($packages_ref, %opts) = @_;
-
-    my $filters_ref = create_filters( %opts );
-    filtration( $filters_ref, $packages_ref );
-
-    # Not necessary, because we alter reference in place...
-    return $packages_ref;
-}
-
-sub filtration
-{
-    my ($filters_ref, $packages_ref) = @_;
-
-    FILTER:
-    for my $filter ( @$filters_ref ) {
-        @$packages_ref = grep { $filter->() } @$packages_ref;
-        # Stop if our array is now empty...
-        last FILTER unless scalar @$packages_ref;
+    FINDOBJ_LOOP:
+    for my $objname ( @$args_ref ) {
+        my @objs = $lookup_ref->( $objname );
+        unless ( @objs ) {
+            __PACKAGE__->error( sprintf $param{'error'}, $objname );
+            next FINDOBJ_LOOP;
+        }
+        push @found, @objs;
     }
 
-    return;
+    return @found;
+}
+
+##----------------------------------------------------------------------------
+## PRINTERS
+##----------------------------------------------------------------------------
+
+sub create_printer
+{
+    my ($class, %opts) = @_;
+
+    my $printer_result;
+
+    my $append = sub {
+        my $newprinter = shift;
+        my $oldprinter = $printer_result || sub { };
+        $printer_result = sub {
+            my $obj = shift;
+            $newprinter->( $obj );
+            $oldprinter->( $obj );
+        }
+    };
+
+    my $printer = sub {
+        my $optname = shift;
+        return unless $opts{ $optname };
+        $append->( shift );
+    };
+
+    $printer->( 'info' => \&_print_info );
+    $printer->( 'changelog' => sub {
+                    my $pkg = shift;
+                    my $changelog = $pkg->changelog;
+                    if ( $changelog ) {
+                        print $changelog;
+                    }
+                    else {
+                        my $name = $pkg->name;
+                        __PACKAGE__->error
+                            ( q{no changelog available for } .
+                              qq{'$name'} );
+                    }
+                } );
+
+    return $printer_result;
+}
+
+sub _print_info
+{
+    my $pkg = shift;
+
+    my %info = $pkg->attribs;
+    $info{'desc'} = Text::Wrap::wrap( 'Description    : ',
+                                      '                 ',
+                                      $info{'desc'} );
+
+    for my $pluralkey ( grep { /s\z/ } keys %info ) {
+        my $aref = $info{ $pluralkey };
+        $info{ $pluralkey } = ( @$aref ? join q{ }, @$aref : 'None' );
+    }
+
+    $info{'reason'}        = ucfirst $info{'reason'} . 'ly installed.';
+    $info{'has_scriptlet'} = ( $info{'has_scriptlet'} ? 'Yes' : 'No' );
+
+    return <<"END_INFO";
+Name           : $info{'name'}
+Version        : $info{'version'}
+URL            : $info{'url'}
+Licenses       : $info{'licenses'}
+Groups         : $info{'groups'}
+Provides       : $info{'provides'}
+Depends On     : $info{'depends'}
+Optional Deps  : $info{'optdepends'}
+Required By    : $info{'requiredby'}
+Conflicts With : $info{'conflicts'}
+Replaces       : $info{'replaces'}
+Installed Size : $info{'isize'}
+Packager       : $info{'packager'}
+Architecture   : $info{'arch'}
+Build Date     : $info{'builddate'}
+Install Date   : $info{'installdate'}
+Install Reason : $info{'reason'}
+Install Script : $info{'has_scriptlet'}
+$info{'desc'};
+END_INFO
+}
+
+##----------------------------------------------------------------------------
+## FILTERS
+##----------------------------------------------------------------------------
+
+# Creates a closure that will run all filters on $_ (a package object)
+# The closure returns 1 if it is not filtered out, 0 if it is.
+sub create_filter
+{
+    my ($class, %opts) = @_;
+
+    my @filters;
+    my $filter = sub {
+        my ($optname, $filter_ref) = @_;
+        return unless $opts{ $optname };
+        push @filters, $filter_ref;
+    };
+
+    $filter->( 'deps'       => sub { $_->reason eq 'implicit' } );
+    $filter->( 'explicit'   => sub { $_->reason eq 'explicit' } );
+    $filter->( 'foreign'    => \&_filter_foreign );
+    $filter->( 'unrequired' => sub { 0 == @{ $_->compute_requiredby } } );
+    $filter->( 'upgrades'   => \&_filter_upgrades );
+
+    return sub {
+        # $_ is our package object to check!
+        for my $filter ( @filters ) {
+            return 0 unless $filter->();
+        }
+
+        return 1
+    }
 }
 
 
@@ -258,58 +307,6 @@ sub _filter_upgrades
     }
     
     return 0;
-}
-
-sub create_filters
-{
-    my (%opts) = @_;
-
-    my @filters;
-    my $filter = sub {
-        my ($optname, $filter_ref) = @_;
-        return unless $opts{ $optname };
-        push @filters, $filter_ref;
-    };
-
-    $filter->( 'deps'       => sub { $_->reason eq 'implicit' } );
-    $filter->( 'explicit'   => sub { $_->reason eq 'explicit' } );
-    $filter->( 'foreign'    => \&_filter_foreign );
-    $filter->( 'unrequired' => sub { 0 == @{ $_->compute_requiredby } } );
-    $filter->( 'upgrades'   => \&_filter_upgrades );
-
-    return \@filters;
-}
-
-sub help
-{
-    return <<'END_HELP';
-usage:  ppacman {-Q --query} [options] [package(s)]
-options:
-  -c, --changelog      view the changelog of a package
-  -d, --deps           list packages installed as dependencies [filter]
-  -e, --explicit       list packages explicitly installed [filter]
-  -g, --groups         view all members of a package group
-  -i, --info           view package information (-ii for backup files)
-  -k, --check          check that the files owned by the package(s) are present
-  -l, --list           list the contents of the queried package
-  -m, --foreign        list installed packages not found in sync db(s) [filter]
-  -o, --owns <file>    query the package that owns <file>
-  -p, --file <package> query a package file instead of the database
-  -s, --search <regex> search locally-installed packages for matching strings
-  -t, --unrequired     list packages not required by any package [filter]
-  -u, --upgrades       list outdated packages [filter]
-  -q, --quiet          show less information for query and search
-      --config <path>  set an alternate configuration file
-      --logfile <path> set an alternate log file
-      --noconfirm      do not ask for any confirmation
-      --noprogressbar  do not show a progress bar when downloading files
-      --noscriptlet    do not execute the install scriptlet if one exists
-  -v, --verbose        be verbose
-      --debug          display debug messages
-  -r, --root <path>    set an alternate installation root
-  -b, --dbpath <path>  set an alternate database location
-      --cachedir <dir> set an alternate package cache location
-END_HELP
 }
 
 1;
